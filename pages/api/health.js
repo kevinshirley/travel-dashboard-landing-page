7ed6a1cf86fa70a6fd64cbfc5345ce409d7228b2
Kevin Shirley <kevinxshirley@gmail.com>
2020-09-24 19:55:29 -0400

    Handler functions

--- pages/api/health.js
+++ pages/api/health.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/health', (req, res) => {
-  return res.send({
-    success: true,
-    message: 'This API is healty!'
-  });
-});
-
-module.exports = router;
+export default (req, res) => {
+  if (req.method === 'GET') {
+    return res.send({
+      success: true,
+      message: 'This API is healty!'
+    });
+  }
+}
