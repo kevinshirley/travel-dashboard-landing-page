449eb998159392a591d70f9f6043a2633dba2299
Kevin Shirley <kevinxshirley@gmail.com>
2020-09-24 23:24:32 -0400

    App notification on email sent

--- pages/api/users/free-trial.js
+++ pages/api/users/free-trial.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-const {
-  AWS_ACCESS_KEY_ID, 
-  AWS_SECRET_ACCESS_KEY,
-  AWS_REGION,
-} = process.env;
-const AWS = require('aws-sdk');
-const moment = require('moment');
-const uuidv4 = require('../../../server/utils/uuidv4');
-
-const aws_creds = {
-  table_name: 'users',
-  remote_config: {
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    region: AWS_REGION
-  }
-};
-
 export default (req, res) => {
+  require('dotenv').config();
+  const {
+    AWS_ACCESS_KEY_ID, 
+    AWS_SECRET_ACCESS_KEY,
+    AWS_REGION,
+  } = process.env;
+  const AWS = require('aws-sdk');
+  const moment = require('moment');
+  const uuidv4 = require('../../../server/utils/uuidv4');
+
+  const aws_creds = {
+    table_name: 'users',
+    remote_config: {
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+      region: AWS_REGION
+    }
+  };
+
   if (req.method === 'GET') {
     return res.send({
       path: '/api/users/free-trial',
@@ -43,7 +43,7 @@ export default (req, res) => {
       }
     };
 
-    db.put(params, function(err, data) {
+    const dbRes = db.put(params, function(err, data) {
       if (err) {
         console.log('aws error', err);
         return err;
@@ -57,6 +57,7 @@ export default (req, res) => {
     return res.send({
       success: true,
       email: data.email,
+      aws: dbRes,
     });
   }
 };
